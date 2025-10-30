6a639c10afac210bd3efb7c3440f3fd435650500
CallumCX <C.Copping2@newcastle.ac.uk>
2025-10-30 19:25:09 +0000

    added the basic input fields for each of the required fields, and have begun including restrictions where needed. This includes converting it to a jinja template.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, EmailField, SubmitField, StringField
+from wtforms.validators import DataRequired, EqualTo, Length, Email
 
-main = Blueprint('main', __name__)
+main = Blueprint('main', __name__, template_folder='templates')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(),Length(min=3,max=30)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),EqualTo('password')])
+    email = EmailField('Email Address', validators=[DataRequired(),Email()])
+    submit = SubmitField('Login')
 
 @main.route('/', methods=['GET'])
 def home():
@@ -8,5 +18,10 @@ def home():
 
 @main.route('/register', methods=['GET', 'POST'])
 def register():
-    name = request.form.get('name') if request.method == 'POST' else ''
-    return render_template('register.html', name=name)
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        return "successfully registered your account"
+    return render_template('register.html', form=form)
